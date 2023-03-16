fab63cca6d65e948a72227c73c77de1a1b91c3f9
Alan Cashman <nocabbage@gmail.com>
2023-03-16 16:14:57 -0700

    add basic axios requests

--- frontwhichonesreal/src/App.jsx
+++ frontwhichonesreal/src/App.jsx
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const API_URL = "http://localhost:9090/questions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [images, setImages] = useState("");
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  function getQuestions() {
+    axios.get(`${API_URL}`).then((response) => {
+      console.log(response);
+    });
+  }
+
+  function getQuestion() {
+    axios.get(`${API_URL}/:id`).then((response) => {
+      console.log(response);
+    });
+  }
+
+  return <div></div>;
 }
 
 export default App;
