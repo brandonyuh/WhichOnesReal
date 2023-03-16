3e2face7d9b97249b1995825567776f3d791416d
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2023-03-16 16:46:21 -0700

    add index.js code

--- backwhichonesreal/index.js
+++ backwhichonesreal/index.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const questions = require("./routes/questions");
+const port = 9090;
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// app.use("/questions", questions);
+
+app.get("/", (req, res)=>{
+    res.send("i got it")
+})
+
+
+app.listen(port, console.log(`Express is listening to ${port}`));
