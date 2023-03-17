2841491a338a1aac885b44940ddf7c26be89e9ed
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-17 11:39:21 -0700

    lshdfkjsadf

--- frontwhichonesreal/src/components/Header/Header.jsx
+++ frontwhichonesreal/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import qr from "../../assets/images/qr.png"
 import "./Header.scss"
 
 function Header() {
@@ -6,6 +7,7 @@ function Header() {
     <div className="header__container">
       <h1 className="header__title">Which One is Real?</h1>
       <div>
+        <img src={qr} alt="" />
         <h2>
           Go to <a href="https://bit.ly/3JPBMpa">bit.ly/3JPBMpa</a> to vote!
         </h2>
