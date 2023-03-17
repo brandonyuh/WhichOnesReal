3e38d4dadfba7ce813cbea214c68066055dc937e
Gustavo Costa <gustavo.lcostaa@gmail.com>
2023-03-16 17:03:45 -0700

    created first question.json

--- backwhichonesreal/index.js
+++ backwhichonesreal/index.js
@@ -3,6 +3,7 @@ const questions = require("./routes/questions");
 const port = 9090;
 const app = express();
 const cors = require("cors");
+app.use(express.static("public"));
 
 app.use(cors());
 
@@ -15,11 +16,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use("/questions", questions);
+app.use("/questions", questions);
 
-app.get("/", (req, res)=>{
-    res.send("i got it")
-})
-
+app.get("/", (req, res) => {
+  res.send("i got it");
+});
 
 app.listen(port, console.log(`Express is listening to ${port}`));
--- backwhichonesreal/routes/questions.js
+++ backwhichonesreal/routes/questions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const data = "../data/questions.json";
+const data = require("../data/questions.json");
 
 router.get("/", (req, res) => {
   res.json(
