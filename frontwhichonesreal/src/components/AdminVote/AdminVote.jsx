3bafbfecef7ce8c314bf752f404fa8bbc1857625
Brandon <brandonyuh@gmail.com>
2023-03-17 14:41:00 -0700

    Added a barchart at the top left

--- frontwhichonesreal/src/components/AdminVote/AdminVote.jsx
+++ frontwhichonesreal/src/components/AdminVote/AdminVote.jsx
@@ -1,11 +1,35 @@
 import "./AdminVote.scss";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js";
+import { Bar } from "react-chartjs-2";
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
 function AdminVote() {
   const voteURL = "http://10.1.230.212:9090/vote/";
   const millisInterval = 1 * 1000;
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+
+  const data = {
+    datasets: [
+      {
+        label: "Left",
+        data: { left },
+        backgroundColor: "aqua",
+        borderColor: "black",
+        borderWidth: 1,
+      },
+      {
+        label: "Right",
+        data: { right },
+        backgroundColor: "red",
+        borderColor: "black",
+        borderWidth: 1,
+      },
+    ],
+  };
+  const options = {};
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -29,13 +53,16 @@ function AdminVote() {
   return (
     <>
       <div className="container">
-      <h1 className="title">Results</h1>
-
-      <span className="span">Left: {left}</span> <span className="span">Right: {right}</span>
-      
-      <div>
-        <button className = "btn" onClick={resetNumbers}>Reset</button>
+        <h1 className="title">Results</h1>
+        <span className="span">Left: {left}</span> <span className="span">Right: {right}</span>
+        <div>
+          <button className="btn" onClick={resetNumbers}>
+            Reset
+          </button>
+        </div>
       </div>
+      <div className="vote__chart">
+        <Bar className="bar-chart" style={{ padding: "20px", width: "100%" }} data={data} options={options}></Bar>
       </div>
     </>
   );
